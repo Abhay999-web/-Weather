cd7d02e2710394589befaf358fcea80200b7219f
Abhay999-web <singhabhaypratap609@gmail.com>
2025-01-31 10:45:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+const input= document.querySelector("input");
+const btn= document.getElementById("btn");
+const icon= document.querySelector(".icon");
+const weather= document.querySelector(".weather");
+const temperature= document.querySelector(".temperature");
+const description= document.querySelector(".description");
+
+
+
+
+// btn.addEventListener('click',()=>{
+
+//     console.log("clicked")
+// })
+
+
+
+btn.addEventListener('click',()=>{
+
+    console.log("clicked")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const apikey 3b1430a4d13871dfe1e42f12455dc29d
+
+
+
+
+
+
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
